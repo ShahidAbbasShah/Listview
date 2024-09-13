e81993e72529a1ab09d278ba9a15bfd5dbf1f470
ShahidAbbas <shahidbukhari113@gmail.com>
2024-09-12 22:50:20 -0700

    MyWiget

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,28 @@
+// ignore_for_file: prefer_const_constructors, unused_local_variable
+
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp((MyApp()));
+import 'package:one/myWidget.dart';
+
+
+
+
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
-   MyApp({super.key});
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
-  var arrayData=[
-    {
-      'image':'Asset/hoodie.png',
-      'name':'Shahid',
-      'occupation':'flutter developer',
-      'number':'0344-9999',
-    },
-    {
-    'image':'Asset/hoodie.png',
-      
-    'name':'Ibrahim',
-      'occupation':'junior flutter developer',
-      'number':'0345-9999',
-    },
-    {
-    'image':'Asset/hoodie.png',
-      
-      'name':'Mohammad',
-      'occupation':'senior flutter developer',
-      'number':'0344-8888',
-    },
-    {
-      'image':'Asset/hoodie.png',
-      'name':'Rizwan',
-      'occupation':'Web developer',
-      'number':'0344-7777',
-    },
-    {
-      'image':'Asset/hoodie.png',
-      'name':'Ammar',
-      'occupation':'Website developer',
-      'number':'0336-9999',
-    },
-    {
-      'image':'Asset/hoodie.png',
-      'name':'Hamza',
-      'occupation':'full stack web developer',
-      'number':'03434-3943',
-    }
-  ];
   Widget build(BuildContext context) {
-    final height=MediaQuery.of(context).size.height;
-    final width=MediaQuery.of(context).size.width;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:  ListView(
-            children: arrayData.map((value){
-              return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Container(
-                  height: height*0.15,
-                  width: width*0.4,
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                  borderRadius: BorderRadius.circular(12)
-                  ),
+    return  MaterialApp(
 
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ListTile(
-                        leading: Icon(Icons.perm_identity,size: 45,),
-                        title: Text(value['name'].toString(),style: TextStyle(
-                          fontSize: 20,fontWeight: FontWeight.bold
-                        )),
-                        subtitle: Text(value['occupation'].toString(),style: TextStyle(
-                            fontSize: 15,fontWeight: FontWeight.bold
-                        )),
-                        trailing: CircleAvatar(
-                          radius: 35,
-                          backgroundImage: AssetImage(value['image'].toString()),
-                          
-                                                ),
-                      ),
-                    ),
-                  ),
-                );
-
-    },
-          ).toList()
-        ),
-      ),
-      );
+            debugShowCheckedModeBanner: false,
+            home: MyWidget(),
+          );
 
   }
+
 }
